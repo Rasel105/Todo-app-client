7ebfba9d6d3c9c941da6154f415b5fc5de58b32c
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-19 04:19:40 +0600

    require auth added

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
 
@@ -16,6 +17,7 @@ const Navbar = () => {
 
     const logout = () => {
         signOut(auth);
+        toast.success("Sign Out")
     };
 
     return (
